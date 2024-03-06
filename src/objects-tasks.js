974e1890473c8b09aed410c1078c73e36af98195
Adam Malec <add.malec@gmail.com>
2024-03-06 16:54:12 +0300

    complete: sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -162,8 +162,14 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let amount = 0;
+
+  return queue.every((item) => {
+    amount += 25;
+    const rest = item - 25;
+    return amount >= rest;
+  });
 }
 
 /**
